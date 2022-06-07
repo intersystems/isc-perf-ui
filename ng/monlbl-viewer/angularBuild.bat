5e310844a676122ee4c2adfd8d00614d650fc9e2
isc-tleavitt <73311181+isc-tleavitt@users.noreply.github.com>
2022-06-07 16:46:36 -0400

    Remove generated files

--- ng/monlbl-viewer/angularBuild.bat
+++
@@ -1,3 +0,0 @@
-cd /D C:\Users\tleavitt\Perforce\tleavitt_USE7420tleavitt_7785\projects\AppServices\OpenExchange\zpm\isc.perf.ui\latest\ng\monlbl-viewer\
-call npm ci
-call npx ng build --baseHref=/csp/user/monlbl-viewer/
