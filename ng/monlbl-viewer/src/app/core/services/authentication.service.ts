b1a80fd64ecf9ef084a67cec2100390b2bea7902
Chris Ge <cge@intersystems.com>
2024-08-08 10:09:16 -0400

    fix: logout reroutes to the home page

--- ng/monlbl-viewer/src/app/core/services/authentication.service.ts
+++ ng/monlbl-viewer/src/app/core/services/authentication.service.ts
@@ -1,17 +1,31 @@
 import { Injectable } from '@angular/core';
 import { DefaultService } from 'src/app/generated';
-
+import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
   constructor(
       private defaultService: DefaultService,
+      private router: Router,
   ) {}
 
   logout() {
-      this.defaultService.authLogoutPost().subscribe() //end iris session (because of shared cookies, does for all CSP applications)
-      window.location.reload(); // forces a hard reload to re-show the iris login page
+    //end iris session (because of shared cookies, does for all CSP applications)
+      this.defaultService.authLogoutPost().subscribe({
+        next: () => {
+          let url = window.location.href;
+          // Remove everything after "monlbl-viewer/"
+          const targetSegment = 'monlbl-viewer/';
+          const targetIndex = url.indexOf(targetSegment);
+
+          if (targetIndex !== -1) {
+            url = url.substring(0, targetIndex + targetSegment.length);
+          }
+          window.location.href = url; 
+        }
+      }) 
+      
   }
 
 }
